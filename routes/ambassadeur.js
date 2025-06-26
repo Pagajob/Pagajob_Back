0cabc8f3cd077cfe531d18de5f933e74b3ecc26e
Louis <louis.ferret@etu.univ-lyon1.fr>
2025-06-26 22:14:01 +0200

    Erreur 404 dashboard Ambassador

--- routes/ambassadeur.js
+++ routes/ambassadeur.js
@@ -1,7 +1,10 @@
 // api/routes/ambassador.js
 import express from 'express';
 import { getAmbassadorStats } from '../controllers/ambassadeur.js';
+import { verifyToken } from '../middlewares/auth.js';
 
 const router = express.Router();
-router.get('/stats', getAmbassadorStats);
+
+router.get('/stats', verifyToken, getAmbassadorStats);
+
 export default router;
